4a5e615a723d246bb79d4404e5827d3b9e59ddc8
Thomas Korthuis <thomas@move2create.com>
2016-07-07 10:56:09 -0400

    Initial commit of init script and thingspeak user script

---
+++ init.lua
@@ -0,0 +1,13 @@
+print("Setting up WIFI...")
+wifi.setmode(wifi.STATION)
+--modify according your wireless router settings
+wifi.sta.config("SSID","KEY")
+wifi.sta.connect()
+tmr.alarm(1, 1000, 1, function()
+if wifi.sta.getip()== nil then
+else
+tmr.stop(1)
+print("Config done, IP is "..wifi.sta.getip())
+dofile("dht11.lua")
+end
+end)
